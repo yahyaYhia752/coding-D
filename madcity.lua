b43bb29f76073c963c8fc873c49c878b9f7e9415
yahyaYhia752 <123502034+yahyaYhia752@users.noreply.github.com>
2024-10-05 23:17:40 +0300

    madcity.lua

---
+++ madcity.lua
@@ -0,0 +1,110 @@
+-- mute sounds
+for i,v in pairs(game:GetDescendants()) do
+	if v:IsA("Sound") then
+		v:Stop()
+	end
+end
+-- // GUI TO LUA \\ --
+if _G.ShiftLock then
+	game.Players.LocalPlayer.PlayerGui:WaitForChild("shift lock"):Destroy()
+end
+_G.ShiftLock = true
+-- // INSTANCES: 2 | SCRIPTS: 1 | MODULES: 0 \\ --
+
+local UI = {}
+
+-- // StarterGui.shift lock \\ --
+UI["1"] = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"))
+UI["1"]["Enabled"] = false
+UI["1"]["Name"] = [[shift lock]]
+UI["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling
+UI["1"]["ResetOnSpawn"] = true
+
+-- // StarterGui.shift lock.Script \\ --
+UI["2"] = Instance.new("LocalScript", UI["1"])
+UI["2"]["Name"] = [[Script]]
+
+-- // StarterGui.shift lock.Script \\ --
+local function SCRIPT_2()
+local script = UI["2"]
+
+	local blackListTools = {
+		"Phone";
+	}
+	local offest = Vector3.new(1.75,1,0)
+	local Button2offest = Vector3.new(-.75,0,0)
+	
+	local cam = workspace.CurrentCamera
+	local plr = game.Players.LocalPlayer
+	local char = plr.Character
+	local uis = game.UserInputService
+	local h:Humanoid = char:WaitForChild("Humanoid")
+	local hrp:Part = char:WaitForChild("HumanoidRootPart")
+	local ShiftLockCFrame = Instance.new("CFrameValue")
+	ShiftLockCFrame.Parent = script
+	ShiftLockCFrame.Name = "ShiftLockCFrame"
+	boolValue = false
+	zoom = false
+	input = Enum.KeyCode.LeftShift
+	ti = TweenInfo.new(.75,Enum.EasingStyle.Quint,Enum.EasingDirection.Out)
+	local function shift(boolean)
+		local upv_cframe
+		if zoom then
+			upv_cframe = CFrame.new(offest+ Button2offest)
+		else
+			upv_cframe = CFrame.new(offest)
+		end
+		if boolean then
+			game.TweenService:Create(ShiftLockCFrame,ti,{Value = upv_cframe}):Play()
+		else
+			game.TweenService:Create(ShiftLockCFrame,ti,{Value = CFrame.new(Vector3.new(0,0,0))}):Play()
+		end
+	end
+	--uis.InputBegan:Connect(function(k)
+	--	if k.KeyCode == input then
+	--		boolValue = not boolValue
+	--		shift(boolValue)
+	--	end
+	--end)
+	
+	char.ChildAdded:Connect(function(child)
+		if child:IsA("Tool") then
+			if table.find(blackListTools,child.Name) then return nil end
+			boolValue = true
+			shift(boolValue)
+		end
+	end)
+	char.ChildRemoved:Connect(function(child)
+		if child:IsA("Tool") then
+			if table.find(blackListTools,child.Name) then return nil end
+			boolValue = false
+			shift(boolValue)
+		end
+	end)
+	plr:GetMouse().Button2Up:Connect(function()
+		zoom = false
+		shift(boolValue)
+	end)
+	plr:GetMouse().Button2Down:Connect(function()
+		if boolValue then
+			zoom = true
+		else
+			zoom = false
+		end
+		shift(boolValue)
+	end)
+	game["Run Service"].RenderStepped:Connect(function()
+		cam.CFrame = cam.CFrame * ShiftLockCFrame.Value
+		-- character rotate
+		h.AutoRotate = not boolValue
+		if boolValue then
+			hrp.CFrame = CFrame.new(hrp.Position,hrp.Position + cam.CFrame.LookVector * Vector3.new(1,0,1))
+			-- uis.MouseBehavior = Enum.MouseBehavior.LockCenter
+		else
+			-- uis.MouseBehavior = Enum.MouseBehavior.Default
+		end
+	end)
+end
+task.spawn(SCRIPT_2)
+
+return UI["1"], require;
