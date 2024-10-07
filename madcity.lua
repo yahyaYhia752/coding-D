37b01cb291ae0a32423787d7d38a2704f78e1e54
yahyaYhia752 <123502034+yahyaYhia752@users.noreply.github.com>
2024-10-07 04:16:04 +0300

    Update madcity.lua

--- madcity.lua
+++ madcity.lua
@@ -5,111 +5,117 @@ for i,v in pairs(game:GetDescendants()) do
 	end
 end
 -- doing task
+-- waiting other loading
+
 if _G.ShiftLock then
 	return nil
 end
 _G.ShiftLock = true
-
---local function SCRIPT_2()
---script = Instance.new("LocalScript")
---script.Parent = game.CoreGui
---script.Name = "shift lock"
--- waiting other loading
+print("script Start")
 repeat wait() until
 game.Players.LocalPlayer
 	and game.Players.LocalPlayer.Character
 	and game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
 	and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
 
-local blackListTools = {
-	"Phone";
-}
-local offest = Vector3.new(1.75,1,0)
-local Button2offest = Vector3.new(-.75,0,0)
-local dataFolder = Instance.new("Folder",game.ReplicatedStorage)
-dataFolder.Name = tostring((math.random(1,9999)*math.random(1,9999))/9999)
-local cam = workspace.CurrentCamera
-local plr = game.Players.LocalPlayer
-local char = plr.Character
-local uis = game.UserInputService
-local h:Humanoid = char:WaitForChild("Humanoid")
-local hrp:Part = char:WaitForChild("HumanoidRootPart")
-local ShiftLockCFrame = Instance.new("CFrameValue")
-ShiftLockCFrame.Parent = dataFolder
-ShiftLockCFrame.Name = "ShiftLockCFrame"
-boolValue = false
-zoom = false
-input = Enum.KeyCode.LeftShift
-ti = TweenInfo.new(.75,Enum.EasingStyle.Quint,Enum.EasingDirection.Out)
+local function SCRIPT_2()
+	--script = Instance.new("LocalScript")
+	--script.Parent = game.CoreGui
+	--script.Name = "shift lock"
 
-
-local function shift(boolean)
-	local upv_cframe
-	if zoom then
-		upv_cframe = CFrame.new(offest+ Button2offest)
-	else
-		upv_cframe = CFrame.new(offest)
-	end
-	if boolean then
-		game.TweenService:Create(ShiftLockCFrame,ti,{Value = upv_cframe}):Play()
-	else
-		game.TweenService:Create(ShiftLockCFrame,ti,{Value = CFrame.new(Vector3.new(0,0,0))}):Play()
-	end
-end
-
-char.ChildAdded:Connect(function(child)
-	if child:IsA("Tool") then
-		if table.find(blackListTools,child.Name) then return nil end
-		boolValue = true
-		shift(boolValue)
-	end
-end)
-char.ChildRemoved:Connect(function(child)
-	if child:IsA("Tool") then
-		if table.find(blackListTools,child.Name) then return nil end
-		boolValue = false
-		shift(boolValue)
-	end
-end)
-h.Changed:Connect(function()
-	if h.Health <= 0 then -- if player is died already
-		boolValue = false
-		zoom = false
-		return nil 
-	end
-	if h.Sit == true then
-		boolValue = false
-		shift(boolValue)
-	end
-end)
-plr:GetMouse().Button2Up:Connect(function()
-	zoom = false
-	shift(boolValue)
-end)
-plr:GetMouse().Button2Down:Connect(function()
-	if boolValue then
-		zoom = true
-	else
-		zoom = false
-	end
-	shift(boolValue)
-end)
-h.Died:Connect(function()
+	local blackListTools = {
+		"Phone";
+	}
+	local offest = Vector3.new(1.75,1,0)
+	local Button2offest = Vector3.new(-.75,0,0)
+	local dataFolder = Instance.new("Folder",game.ReplicatedStorage)
+	dataFolder.Name = tostring((math.random(1,9999)*math.random(1,9999))/9999)
+	local cam = workspace.CurrentCamera
+	local plr = game.Players.LocalPlayer
+	local char = plr.Character
+	local uis = game.UserInputService
+	local h:Humanoid = char:WaitForChild("Humanoid")
+	local hrp:Part = char:WaitForChild("HumanoidRootPart")
+	local ShiftLockCFrame = Instance.new("CFrameValue")
+	ShiftLockCFrame.Parent = dataFolder
+	ShiftLockCFrame.Name = "ShiftLockCFrame"
 	boolValue = false
 	zoom = false
-	game.TweenService:Create(ShiftLockCFrame,ti,{Value = CFrame.new(Vector3.new(0,0,0))}):Play()
-	--shift(boolValue)
-end)
-game["Run Service"].RenderStepped:Connect(function()
-	cam.CFrame = cam.CFrame * ShiftLockCFrame.Value -- change position camera
-	-- character rotate
-	h.AutoRotate = not boolValue
-	if boolValue then -- rotate character
-		hrp.CFrame = CFrame.new(hrp.Position,hrp.Position + cam.CFrame.LookVector * Vector3.new(1,0,1))
-		-- uis.MouseBehavior = Enum.MouseBehavior.LockCenter
-	else
-		-- uis.MouseBehavior = Enum.MouseBehavior.Default
+	input = Enum.KeyCode.LeftShift
+	ti = TweenInfo.new(.75,Enum.EasingStyle.Quint,Enum.EasingDirection.Out)
+
+
+	local function shift(boolean)
+		local upv_cframe
+		if zoom then
+			upv_cframe = CFrame.new(offest+ Button2offest)
+		else
+			upv_cframe = CFrame.new(offest)
+		end
+		if boolean then
+			game.TweenService:Create(ShiftLockCFrame,ti,{Value = upv_cframe}):Play()
+		else
+			game.TweenService:Create(ShiftLockCFrame,ti,{Value = CFrame.new(Vector3.new(0,0,0))}):Play()
+		end
 	end
+
+	char.ChildAdded:Connect(function(child)
+		if child:IsA("Tool") then
+			if table.find(blackListTools,child.Name) then return nil end
+			boolValue = true
+			shift(boolValue)
+		end
+	end)
+	char.ChildRemoved:Connect(function(child)
+		if child:IsA("Tool") then
+			if table.find(blackListTools,child.Name) then return nil end
+			boolValue = false
+			shift(boolValue)
+		end
+	end)
+	h.Changed:Connect(function()
+		if h.Health <= 0 then -- if player is died already
+			boolValue = false
+			zoom = false
+			return nil 
+		end
+		if h.Sit == true then
+			boolValue = false
+			shift(boolValue)
+		end
+	end)
+	plr:GetMouse().Button2Up:Connect(function()
+		zoom = false
+		shift(boolValue)
+	end)
+	plr:GetMouse().Button2Down:Connect(function()
+		if boolValue then
+			zoom = true
+		else
+			zoom = false
+		end
+		shift(boolValue)
+	end)
+	h.Died:Connect(function()
+		dataFolder:Destroy()
+		boolValue = false
+		zoom = false
+		game.TweenService:Create(ShiftLockCFrame,ti,{Value = CFrame.new(Vector3.new(0,0,0))}):Play()
+		--shift(boolValue)
+	end)
+	game["Run Service"].RenderStepped:Connect(function()
+		cam.CFrame = cam.CFrame * ShiftLockCFrame.Value -- change position camera
+		-- character rotate
+		h.AutoRotate = not boolValue
+		if boolValue then -- rotate character
+			hrp.CFrame = CFrame.new(hrp.Position,hrp.Position + cam.CFrame.LookVector * Vector3.new(1,0,1))
+			-- uis.MouseBehavior = Enum.MouseBehavior.LockCenter
+		else
+			-- uis.MouseBehavior = Enum.MouseBehavior.Default
+		end
+	end)
+end
+task.spawn(SCRIPT_2)
+game.Players.LocalPlayer.CharacterAdded:Connect(function()
+	task.spawn(SCRIPT_2)
 end)
---end
---task.spawn(SCRIPT_2)
